7b6b19617e3d2ed5cb2f5d4204fcd158329268b6
Rogier De Nys <rogierdenys@gmail.com>
2023-02-28 00:08:31 +0100

    rrc filter

--- main.m
+++ main.m
@@ -3,15 +3,15 @@ close all
 
 % random bitsstream generator
 bit_stream = [];
-for i = 1:300
+for i = 1:30
     bit_stream = [bit_stream round(rand())];
 end
 
 %bit_stream = [0 0 1 1 1 1]
 %output = BPSK(1, bit_stream);
-%output = QPSK(1, bit_stream);
+output = QPSK(1, bit_stream);
 %output = QAM_16(1, bit_stream);
-output = QAM_64(1, bit_stream);
+%output = QAM_64(1, bit_stream);
 
 
 if (length(output) == 0)
@@ -26,6 +26,6 @@ else
 
 end
 Nyquist(output,100)
-real(output)
-imag(output)
+real(output);
+imag(output);
 
