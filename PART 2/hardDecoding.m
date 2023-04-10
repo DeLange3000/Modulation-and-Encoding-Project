766786254f7024ee9ae54e6384e64b057082b67b
DeLange3000 <warre@dewidewa.be>
2023-04-10 11:53:51 +0200

    implemented iterations (not sure if soft decoding is correct -> have to test with BER)

--- PART 2/hardDecoding.m
+++ PART 2/hardDecoding.m
@@ -1,37 +1,39 @@
-function [dec_bs] = hardDecoding(H, rec_bs)
+function [dec_bs] = hardDecoding(H, rec_bs, number_of_iterations)
     [M, N] = size(H);
     rec_bs_l = length(rec_bs);
     
     dec_bs = [];
 
     for block_i = 1:N:rec_bs_l
-        block = rec_bs(block_i:block_i+N-1);
-
-        v_nodes = zeros(M+1, N);
-        v_nodes(1, :) = block;
-        
-        for i = 1:M
-            % #rogier is genius
-            arr = nonzeros(H(i,:) .* (1:N))';
-
-            for j = arr
-                arr_sel = arr(arr~=j);
-                selected = block(arr_sel);
-                if mod(sum(selected), 2) == 1
-                    v_nodes(i+1, j) = 1;
+        for a = 1:number_of_iterations
+            block = rec_bs(block_i:block_i+N-1);
+    
+            v_nodes = zeros(M+1, N);
+            v_nodes(1, :) = block;
+            
+            for i = 1:M
+                % #rogier is genius
+                arr = nonzeros(H(i,:) .* (1:N))';
+    
+                for j = arr
+                    arr_sel = arr(arr~=j);
+                    selected = block(arr_sel);
+                    if mod(sum(selected), 2) == 1
+                        v_nodes(i+1, j) = 1;
+                    end
                 end
             end
+            %get non_zero indices of H
+    
+            for i = 1:N
+            H_nonzero = H(:,i)'.*(2:M+1); % add index due to offset in v_nodes
+            H_nonzero = nonzeros(H_nonzero);
+            cor_block = sum(v_nodes([1; H_nonzero],i));
+            cor_block = round(cor_block/(length(H_nonzero)+1));
+            block(i) = cor_block;
+            end
         end
-        %get non_zero indices of H
-
-        for i = M+1:N
-        H_nonzero = H(:,i)'.*(2:M+1); % add index due to offset in v_nodes
-        H_nonzero = nonzeros(H_nonzero);
-        cor_block = sum(v_nodes([1; H_nonzero],i));
-        cor_block = round(cor_block/(length(H_nonzero)+1));
-        dec_block = cor_block;
-        dec_bs = [dec_bs dec_block];
-        end
+        dec_bs = [dec_bs block(M+1:N)];
     end
     
 end
--- PART 2/softDecoding.m
+++ PART 2/softDecoding.m
@@ -1,4 +1,4 @@
-function [dec_bs] = softDecoding(H,rec_bs, N0)
+function [dec_bs] = softDecoding(H,rec_bs, N0, number_of_iterations)
 
 [M, N] = size(H);
 rec_bs_l = length(rec_bs);
@@ -16,26 +16,28 @@ dec_bs = [];
         end
         
         % loop from here 10 times
-
-        %make L_r
-        for j = 1:N
-            for a = nonzeros(H_index(:,j))' %loops over non zero H indices
-            y_index = 1:N;
-            y_index = y_index(y_index~=j); %j itself cannot be included
-                if(isempty(nonzeros(L_q(a, y_index)))) %if only zero elements are present, make L_r zero
-                    L_r(a,j) = 0;
-                else
-                    L_r(a,j) = prod(sign(nonzeros(L_q(a, y_index))), 'all')*min(nonzeros(abs(L_q(a, y_index))), [], 'all');  %create L matrix
+        
+        for b = 1:number_of_iterations
+            %make L_r
+            for j = 1:N
+                for a = nonzeros(H_index(:,j))' %loops over non zero H indices
+                y_index = 1:N;
+                y_index = y_index(y_index~=j); %j itself cannot be included
+                    if(isempty(nonzeros(L_q(a, y_index)))) %if only zero elements are present, make L_r zero
+                        L_r(a,j) = 0;
+                    else
+                        L_r(a,j) = prod(sign(nonzeros(L_q(a, y_index))), 'all')*min(nonzeros(abs(L_q(a, y_index))), [], 'all');  %create L matrix
+                    end
                 end
             end
-        end
-        
-        %add L_r to L_q
-        for j = 1:N
-            for a = nonzeros(H_index(:, j))'
-                x_index = 1:M;
-                x_index = x_index(x_index~=a); %j itself cannot be included
-                L_q(a,j) =  L_q(a,j) + sum(L_r(x_index,j));
+            
+            %add L_r to L_q
+            for j = 1:N
+                for a = nonzeros(H_index(:, j))'
+                    x_index = 1:M;
+                    x_index = x_index(x_index~=a); %j itself cannot be included
+                    L_q(a,j) =  L_q(a,j) + sum(L_r(x_index,j));
+                end
             end
         end
         
